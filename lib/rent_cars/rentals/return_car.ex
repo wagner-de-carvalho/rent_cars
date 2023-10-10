1947eca708e063be924a1aeb39a273dc11645f24
mindlab <wagncarv@outlook.com>
2023-10-09 22:30:35 -0300

    Tests for return car, calculate fees

--- lib/rent_cars/rentals/return_car.ex
+++ lib/rent_cars/rentals/return_car.ex
@@ -3,11 +3,12 @@ defmodule RentCars.Rentals.ReturnCar do
   alias RentCars.Rentals.Rental
   alias RentCars.Repo
 
-  defstruct rental: nil, days: nil, delay: nil
+  defstruct rental: nil, days: nil, delay: nil, total_fees: nil
 
   def execute(rental_id, user_id) do
     with %__MODULE__{} = return_rental <- get_rental(rental_id, user_id),
-         return_rental <- calculate_delay(return_rental) do
+         return_rental <- calculate_delay(return_rental),
+         return_rental <- calculate_fees(return_rental) do
       return_rental
     else
       error -> error
@@ -31,7 +32,14 @@ defmodule RentCars.Rentals.ReturnCar do
   defp calculate_delay(%{rental: rental} = return_car) do
     now = NaiveDateTime.utc_now()
     days = Timex.diff(now, rental.start_date, :days)
+    days = (days <= 0 && 1) || days
     delay = Timex.diff(now, rental.expected_return_date, :days)
     %{return_car | days: days, delay: delay}
+  end
+
+  defp calculate_fees(%{rental: %{car: car}, delay: delay, days: days} = return_car) do
+    calculate_fine = (delay > 0 && delay * car.fine_amount.amount) || 0
+    total_fees = days * car.daily_rate + calculate_fine
+    %{return_car | total_fees: total_fees}
   end
 end
--- test/rent_cars/rentals/return_car_test.exs
+++ test/rent_cars/rentals/return_car_test.exs
@@ -34,5 +34,69 @@ defmodule RentCars.Rentals.ReturnCarTest do
 
       assert %{days: 7, delay: 5} = ReturnCar.execute(rental.id, user.id)
     end
+
+    test "should not return negative or zero days", %{car: car, user: user} do
+      today = Date.utc_today()
+      expected_return_date = Timex.shift(today, days: +5) |> Timex.to_naive_datetime()
+      start_date = Timex.to_naive_datetime(today)
+
+      rental =
+        rental_fixture(%{
+          car_id: car.id,
+          user_id: user.id,
+          expected_return_date: expected_return_date,
+          start_date: start_date
+        })
+
+      assert %{days: 1, delay: -4} = ReturnCar.execute(rental.id, user.id)
+    end
+
+    test "calculates total fees", %{car: car, user: user} do
+      today = Date.utc_today()
+      expected_return_date = Timex.shift(today, days: +5) |> Timex.to_naive_datetime()
+      start_date = Timex.to_naive_datetime(today)
+
+      rental =
+        rental_fixture(%{
+          car_id: car.id,
+          user_id: user.id,
+          expected_return_date: expected_return_date,
+          start_date: start_date
+        })
+
+      assert %{total_fees: 100} = ReturnCar.execute(rental.id, user.id)
+    end
+
+    test "calculates total fees for 3 days", %{car: car, user: user} do
+      today = Date.utc_today()
+      expected_return_date = Timex.shift(today, days: +5) |> Timex.to_naive_datetime()
+      start_date = Timex.shift(today, days: -3) |> Timex.to_naive_datetime()
+
+      rental =
+        rental_fixture(%{
+          car_id: car.id,
+          user_id: user.id,
+          expected_return_date: expected_return_date,
+          start_date: start_date
+        })
+
+      assert %{total_fees: 300} = ReturnCar.execute(rental.id, user.id)
+    end
+
+    test "calculates total fees with fine amount", %{car: car, user: user} do
+      today = Date.utc_today()
+      expected_return_date = Timex.shift(today, days: -3) |> Timex.to_naive_datetime()
+      start_date = Timex.shift(today, days: -6) |> Timex.to_naive_datetime()
+
+      rental =
+        rental_fixture(%{
+          car_id: car.id,
+          user_id: user.id,
+          expected_return_date: expected_return_date,
+          start_date: start_date
+        })
+
+      assert %{total_fees: 690} = ReturnCar.execute(rental.id, user.id)
+    end
   end
 end
